10a030078624a8c75a703a2fd9e74f62169e7ccd
gabrieldaviddiaz <gabrieldaviddiazdiaz2024@gmail.com>
2024-06-05 05:09:29 -0400

    cambios en el codigo

--- src/routes/user.js
+++ src/routes/user.js
@@ -34,8 +34,8 @@ router.put("/users/:id", (req, res) => {
 //eliminar
 router.delete("/users/:id", (req, res) => {
     const { id } = req.params;
-    userSchema.
-        remove({ _id: id }).then((data) => res.json(data)).catch((error) => res.json({ message: "se ha generado un error" }));
+    userSchema
+        .deleteOne({ _id: id }) .then((data) => res.json(data)).catch((error) => res.json({ message: "se ha generado un error" }));
 });
 
 module.exports = router;        
